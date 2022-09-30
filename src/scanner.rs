09b1aaa756ea9610a235109b148c9f927cc4b505
kelim-msft <113640485+kelim-msft@users.noreply.github.com>
2022-09-30 11:07:11 -0700

    scanner

---
+++ src/scanner.rs
@@ -0,0 +1,60 @@
+use std::cmp::min;
+use std::io::{BufWriter, stdin, stdout, Write};
+use std::str;
+const BITS: usize = 19;
+
+#[derive(Default)]
+pub struct Scanner {
+    buffer: Vec<String>
+}
+
+impl Scanner {
+    pub fn next<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+
+    let out = &mut BufWriter::new(stdout());
+    let n = scan.next::<usize>();
+    let q = scan.next::<usize>();
+    let a: Vec<usize> = (0..n).map(|_| scan.next()).collect();
+    let mut closest = vec![n; BITS];
+    let mut next = vec![vec![n; BITS]; n+1];
+
+    for (i, &ai) in a.iter().enumerate().rev() {
+        let (zero_bits, one_bits): (Vec<usize>, Vec<usize>) =
+            (0..BITS).partition(|b| (ai & (1usize << b)) == 0);
+
+        for b in one_bits {
+            next[i][b] = i;
+            let j = closest[b];
+            for &c in &zero_bits { // must borrow (not consume) zero_bits to reuse it!
+                // inner loop's constant factor <= 1/4, so it may run 27 million times
+                next[i][c] = min(next[i][c], next[j][c]);
+            }
+            closest[b] = i;
+        }
+    }
+
+    for _ in 0..q {
+        let x = scan.next::<usize>() - 1;
+        let y = scan.next::<usize>() - 1;
+        let success = (0..BITS).filter(|b| (a[y] & (1usize << b)) != 0)
+            .any(|b| next[x][b] <= y);
+        if success {
+            writeln!(out, "Shi").ok();
+        } else {
+            writeln!(out, "Fou").ok();
+        }
+    }
+}
